ee1c24f775dbc3495e4e04e86dd9976d4a9bb495
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-15 07:01:40 +0800

    Set CartProvider To App Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import CartPage from "./pages/cart/CartPage";
 import Navbar from "./components/Navbar";
+import CartProvider from "./contexts/CartProvider";
 
 export default function App() {
    return (
-      <>
+      <CartProvider>
          <Navbar />
          <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/cart" element={<CartPage />}></Route>
          </Routes>
-      </>
+      </CartProvider>
    );
 }
