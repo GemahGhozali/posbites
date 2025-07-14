c90c3cacc039094311da78ede201389b24cf085d
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-15 07:20:56 +0800

    Implement useCart() Custom Hook In CartPage Component

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
+import useCart from "../../hooks/useCart";
 
 import CartView from "./views/CartView";
 import EmptyCartView from "./views/EmptyCartView";
 
 export default function CartPage() {
-   const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) || []);
-
-   localStorage.setItem("cart", JSON.stringify(cart));
+   const { cart } = useCart();
 
    return (
       <main className="max-w-7xl mx-auto pt-[75px] md:px-4 md:pb-10 md:pt-[114px] h-screen min-h-[550px]">
          <div className="h-full p-2 sm:p-4 bg-slate-50 md:border-gray-300 md:border-2 shadow-xl md:rounded-2xl flex flex-col gap-2 sm:gap-4">
-            {cart.length === 0 ? <EmptyCartView /> : <CartView cart={cart} setCart={setCart} />}
+            {cart.length === 0 ? <EmptyCartView /> : <CartView />}
          </div>
       </main>
    );
