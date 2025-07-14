3cd988787e639f740e78b397cece6d1ea7a7491d
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-15 07:43:34 +0800

    Updating ProductCard Component In /cart Folder : Implement useCart() Custom Hook & Removing Unecessary Props

--- src/pages/cart/components/ProductCard.jsx
+++ src/pages/cart/components/ProductCard.jsx
@@ -1,8 +1,11 @@
 import { formatPriceCurrency } from "../../../utilities/utilities";
+import useCart from "../../../hooks/useCart";
 
 import QuantityChanger from "./QuantityChanger";
 
-export default function ProductCard({ id, image, name, price, quantity, onDelete, onIncreaseQuantity, onDecreaseQuantity }) {
+export default function ProductCard({ id, image, name, price, quantity }) {
+   const { deleteProduct } = useCart();
+
    return (
       <div className="p-2 me-2 sm:me-4 bg-white border-2 border-gray-300 shadow-md rounded-md grid grid-cols-[auto_1fr] gap-2 md:gap-4">
          <img src={image} alt={name} className="aspect-square h-[90px] sm:h-24 bg-slate-300 rounded-sm" />
@@ -12,9 +15,9 @@ export default function ProductCard({ id, image, name, price, quantity, onDelete
                <h4 className="text-lg sm:text-xl font-semibold line-clamp-1">{name}</h4>
                <p className="text-sm sm:text-base">{formatPriceCurrency(price)}</p>
             </div>
-            <QuantityChanger productId={id} quantity={quantity} onIncreaseQuantity={onIncreaseQuantity} onDecreaseQuantity={onDecreaseQuantity} />
+            <QuantityChanger productId={id} quantity={quantity} />
             <span className="max-md:hidden md:col-span-3 font-semibold text-green-600">{formatPriceCurrency(price * quantity)}</span>
-            <button onClick={() => onDelete(id)} className="bx bx-trash max-md:justify-self-end md:col-span-1 bg-green-600 text-white size-[30px] rounded-md grid place-content-center duration-150 hover:bg-green-700"></button>
+            <button onClick={() => deleteProduct(id)} className="bx bx-trash max-md:justify-self-end md:col-span-1 bg-green-600 text-white size-[30px] rounded-md grid place-content-center duration-150 hover:bg-green-700"></button>
          </div>
 
          <hr className="col-span-2 border border-gray-300 md:hidden" />
