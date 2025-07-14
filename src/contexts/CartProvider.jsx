866efedda65394c441717c88bf1810440f91e1f5
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-15 06:57:59 +0800

    Creating CartProvider From CartContext

---
+++ src/contexts/CartProvider.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+import CartContext from "./CartContext";
+
+import products from "../constant/products";
+
+export default function CartProvider({ children }) {
+   const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) || []);
+
+   useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
+   function addProduct(productId) {
+      const isProductExist = cart.some((product) => product.id === productId);
+
+      if (isProductExist) return;
+
+      const product = products.find((product) => product.id === productId);
+      setCart((prevCart) => [...prevCart, { ...product, quantity: 1 }]);
+   }
+
+   function deleteProduct(productId) {
+      setCart((prevCart) => prevCart.filter((product) => product.id !== productId));
+   }
+
+   function increaseProductQuantity(productId) {
+      setCart((prevCart) =>
+         prevCart.map((product) => {
+            return product.id === productId ? { ...product, quantity: product.quantity + 1 } : product;
+         })
+      );
+   }
+
+   function decreaseProductQuantity(productId) {
+      setCart((prevCart) =>
+         prevCart.map((product) => {
+            if (product.id === productId && product.quantity > 1) {
+               return { ...product, quantity: product.quantity - 1 };
+            }
+
+            return product;
+         })
+      );
+   }
+
+   return <CartContext.Provider value={{ cart, addProduct, deleteProduct, increaseProductQuantity, decreaseProductQuantity }}>{children}</CartContext.Provider>;
+}
