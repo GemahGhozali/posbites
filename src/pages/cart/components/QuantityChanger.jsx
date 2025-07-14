eb5c1df68eab97059b4ec8450757de613a22354f
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-14 20:16:42 +0800

    Creating QuantityChanger Component

---
+++ src/pages/cart/components/QuantityChanger.jsx
@@ -0,0 +1,9 @@
+export default function QuantityChanger({ productId, quantity, onIncreaseQuantity, onDecreaseQuantity }) {
+   return (
+      <div className="quantity-updater-btn max-md:col-start-1 md:col-span-3 size-fit flex border-2 border-gray-300 rounded-md overflow-hidden *:size-[27px] *:grid *:place-content-center *:duration-150">
+         <button onClick={() => onDecreaseQuantity(productId)} className="bx bx-minus text-green-600 font-semibold hover:text-white hover:bg-green-600"></button>
+         <div className="border-x-2 border-gray-300 text-sm sm:text-base">{quantity}</div>
+         <button onClick={() => onIncreaseQuantity(productId)} className="bx bx-plus text-green-600 font-semibold hover:text-white hover:bg-green-600"></button>
+      </div>
+   );
+}
