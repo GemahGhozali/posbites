048edcfe48375b44d1e832a36eb8cb0aeac6d8e8
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-15 16:45:28 +0800

    Creating getCurrentDate() Utility Function

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -6,4 +6,13 @@ export function formatPriceCurrency(price) {
    }).format(price);
 }
 
+export function getCurrentDate() {
+   const today = new Date();
+   const day = String(today.getDate()).padStart(2, "0");
+   const month = String(today.getMonth() + 1).padStart(2, "0");
+   const year = today.getFullYear();
+
+   return `${day}-${month}-${year}`;
+}
+
 export * from "./utilities";
