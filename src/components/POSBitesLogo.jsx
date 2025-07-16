c36d96be6eb86e782969214feee3f79d6f3ea30e
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-16 13:03:44 +0800

    Changing POSBitesLogo Component's Line Height

--- src/components/POSBitesLogo.jsx
+++ src/components/POSBitesLogo.jsx
@@ -2,7 +2,7 @@ export default function POSBitesLogo() {
    return (
       <span className="flex items-center">
          <i className="bx bx-menu-right text-green-600 text-[42px] -skew-x-10"></i>
-         <h1 className="text-green-600 font-extrabold text-3xl -skew-x-10">
+         <h1 className="text-green-600 font-extrabold text-3xl leading-0 -skew-x-10">
             POS<span className="text-black">Bites</span>
          </h1>
       </span>
