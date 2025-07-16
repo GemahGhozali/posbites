890afaceb1ddb01245512745cb6255588d74e6c6
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-16 12:59:58 +0800

    Changing Secondary Button Background Color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ color = "primary", className, children, ...props }) {
    const buttonColors = {
       primary: "bg-green-600 text-white hover:bg-green-700",
-      secondary: "bg-white text-green-600 hover:bg-slate-100",
+      secondary: "bg-slate-100 text-green-600 hover:bg-slate-200",
    };
 
    const classes = `px-4 py-2 text-sm sm:text-base font-semibold rounded-md duration-300 ${buttonColors[color]} ${className}`;
