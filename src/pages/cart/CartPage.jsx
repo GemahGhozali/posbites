3899d325cc385871f9d009985c85b0180c2a11a0
Gemah Ghozali <gemahghozali@gmail.com>
2025-07-18 12:03:21 +0800

    Changing Viewport Height To Dynamic Viewport Height

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -8,7 +8,7 @@ export default function CartPage() {
    const { cart } = useCart();
 
    return (
-      <main className="max-w-7xl mx-auto pt-[75px] md:px-4 md:pb-10 md:pt-[114px] h-screen min-h-[550px]">
+      <main className="max-w-7xl mx-auto pt-[75px] md:px-4 md:pb-10 md:pt-[114px] h-dvh min-h-[550px]">
          <div className="h-full p-2 sm:p-4 bg-slate-50 md:border-gray-300 md:border-2 shadow-xl md:rounded-2xl flex flex-col gap-2 sm:gap-4">
             {cart.length === 0 ? <EmptyCartView /> : <CartView />}
          </div>
